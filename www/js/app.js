ec99ddc620d2440c0d178f5a5a1a7d1ace7cdab1
ajstyle <amitjain.lov@gmail.com>
2016-03-13 20:19:00 +0530

    Factory Function Login Service

---
+++ www/js/app.js
@@ -0,0 +1,288 @@
+
+(function(){
+
+
+
+var app = angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material', 'ionMdInput' , 'angularSoap' , 'ngCordova' , 'ngMessages'])
+
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+   //Get uuid 
+
+  var uuid =  window.device.uuid;
+
+  alert(a);
+    
+
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+
+
+.factory("testService", ['$soap',function($soap){
+    var base_url = "http://wcplreg.in/app/appLoginSer1.asmx";
+    var action = "login" ; 
+    return {
+        HelloWorld: function(orderTo,branch,userId,password,mobileNo,deviceID){
+            return $soap.post(base_url, action , {info1 : '125066' ,  info2 : 'A04' , info3 : 'AMIT' , info4 : '1234' , info5 : '917877361402' , info6 : '1234'});
+        }
+    }
+}])
+
+  .directive('validNumber', function() {
+      return {
+        require: '?ngModel',
+        link: function(scope, element, attrs, ngModelCtrl) {
+          if(!ngModelCtrl) {
+            return; 
+          }
+
+          ngModelCtrl.$parsers.push(function(val) {
+            if (angular.isUndefined(val)) {
+                var val = '';
+            }
+            
+            var clean = val.replace(/[^-0-9\.]/g, '');
+            var negativeCheck = clean.split('-');
+            var decimalCheck = clean.split('.');
+            if(!angular.isUndefined(negativeCheck[1])) {
+                negativeCheck[1] = negativeCheck[1].slice(0, negativeCheck[1].length);
+                clean =negativeCheck[0] + '-' + negativeCheck[1];
+                if(negativeCheck[0].length > 0) {
+                    clean =negativeCheck[0];
+                }
+                
+            }
+              
+            if(!angular.isUndefined(decimalCheck[1])) {
+                decimalCheck[1] = decimalCheck[1].slice(0,2);
+                clean =decimalCheck[0] + '.' + decimalCheck[1];
+            }
+
+            if (val !== clean) {
+              ngModelCtrl.$setViewValue(clean);
+              ngModelCtrl.$render();
+            }
+            return clean;
+          });
+
+          element.bind('keypress', function(event) {
+            if(event.keyCode === 32) {
+              event.preventDefault();
+            }
+          });
+        }
+      };
+    })
+
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+    // Turn off caching for demo simplicity's sake
+    $ionicConfigProvider.views.maxCache(0);
+
+    /*
+    // Turn off back button text
+    $ionicConfigProvider.backButton.previousTitleText(false);
+    */
+
+    $stateProvider.state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+   
+     .state('app.register', {
+        url: '/register',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/register.html',
+                controller: 'MainCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-activity" class="button button-fab button-fab-top-right expanded button-energized-900 flap"><i class="icon ion-star"></i></button>',
+                controller: function ($timeout) {
+                    $timeout(function () {
+                        document.getElementById('fab-activity').classList.toggle('on');
+                    }, 200);
+                }
+            }
+        }
+    })
+
+    
+
+    .state('app.gallery', {
+        url: '/gallery',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/gallery.html',
+                controller: 'GalleryCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-gallery" class="button button-fab button-fab-top-right expanded button-energized-900 drop"><i class="icon ion-heart"></i></button>',
+                controller: function ($timeout) {
+                    $timeout(function () {
+                        document.getElementById('fab-gallery').classList.toggle('on');
+                    }, 600);
+                }
+            }
+        }
+    })
+
+    .state('app.login', {
+        url: '/login',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/login.html',
+                controller: 'LoginCtrl'
+            },
+            'fabContent': {
+                template: ''
+            }
+        }
+    })
+
+    .state('app.profile', {
+        url: '/profile',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/profile.html',
+                controller: 'ProfileCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                controller: function ($timeout) {
+                    /*$timeout(function () {
+                        document.getElementById('fab-profile').classList.toggle('on');
+                    }, 800);*/
+                }
+            }
+        }
+    })
+  
+.state('app.debtors', {
+        url: '/debtors',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/debtors.html',
+                controller: 'ProfileCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                controller: function ($timeout) {
+                    /*$timeout(function () {
+                        document.getElementById('fab-profile').classList.toggle('on');
+                    }, 800);*/
+                }
+            }
+        }
+    })
+.state('app.receipt', {
+        url: '/receipt',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/receipt.html',
+                controller: 'ProfileCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                controller: function ($timeout) {
+                    /*$timeout(function () {
+                        document.getElementById('fab-profile').classList.toggle('on');
+                    }, 800);*/
+                }
+            }
+        }
+    })
+.state('app.sales', {
+        url: '/sales',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/sales.html',
+                controller: 'ProfileCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                controller: function ($timeout) {
+                    /*$timeout(function () {
+                        document.getElementById('fab-profile').classList.toggle('on');
+                    }, 800);*/
+                }
+            }
+        }
+    })
+.state('app.creditor', {
+        url: '/Creditors',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/creditors.html',
+                controller: 'ProfileCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                controller: function ($timeout) {
+                    /*$timeout(function () {
+                        document.getElementById('fab-profile').classList.toggle('on');
+                    }, 800);*/
+                }
+            }
+        }
+    })
+    
+.state('app.inventory', {
+        url: '/inventory',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/inventory.html',
+                controller: 'ProfileCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                controller: function ($timeout) {
+                    /*$timeout(function () {
+                        document.getElementById('fab-profile').classList.toggle('on');
+                    }, 800);*/
+                }
+            }
+        }
+    })
+    
+
+
+  .state('app.cheque', {
+        url: '/cheque',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/cheque.html',
+                controller: 'ProfileCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                controller: function ($timeout) {
+                    /*$timeout(function () {
+                        document.getElementById('fab-profile').classList.toggle('on');
+                    }, 800);*/
+                }
+            }
+        }
+    })
+    ;
+  
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/login');
+
+
+
+
+});
+})();
